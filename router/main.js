0e36e5baec18b0685b3be62ad73f7ae513eccc2f
Reza Adhitya <rezaadhitya1995@gmail.com>
2023-02-04 21:29:13 +0700

    added routing and some endpoints

---
+++ router/main.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const router = express.Router()
+
+router.post('/playlist', )          // TODO - Add recommend playlist static method
+router.post('/payment', )           // TODO - Add payment gateway static method
+
+module.exports = router
---
+++ router/users.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+
+router.post('/register', )          // TODO - Add register static method
+router.post('/login', )             // TODO - Add login static method
+router.post('/google-login', )      // TODO - Add google login static method
+router.post('/spotify-login', )     // TODO - Add spotify static method *if time's feasible
+
+module.exports = router
