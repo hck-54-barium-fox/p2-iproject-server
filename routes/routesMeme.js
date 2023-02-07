9e1b72c3e7bb41f6075a4557545cde2620234aef
Hamzah Diza Santoso <hamzah.diza@gmail.com>
2023-02-07 15:31:19 +0700

    add feature getAllMemes

--- routes/routesMeme.js
+++ routes/routesMeme.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const MemeController = require("../controllers/memeController");
+const { authentication } = require("../middleware/auth");
 const router = express();
 
-// router.get("/", MemeController.getAllMemes);
+router.use(authentication);
+router.get("/", MemeController.getAllMemes);
 // router.post("/", MemeController.postGenerateMeme);
 // router.get("/:memeId", MemeController.getMemeById);
 
