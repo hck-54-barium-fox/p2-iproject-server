d883cfc8cf7bd1b6d2254a6f911760039d8a50cd
unknown <wliu970@gmail.com>
2023-02-07 11:20:12 +0700

    initialize project

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const router = require('./routes')
+
+app.use(cors)
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
+app.use(router)
+
+module.exports = app
