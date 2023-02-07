31689833dcf1dd0a33b40b00e46a0f19294ca918
eduardusoldi <eduardusoldi98@gmail.com>
2023-02-07 23:41:06 +0700

    fix: handle different IP Address

--- controllers/controllerPlayer.js
+++ controllers/controllerPlayer.js
@@ -17,7 +17,7 @@ class ControllerPlayer {
             url: `https://api.clashroyale.com/v1/players/%23${req.params.tag}`,
             headers: {
                 Authorization:
-                  "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjYwOTU1ZTliLWJjODEtNGJkZS04ZjVhLWJlNjNlNDg0ODczOSIsImlhdCI6MTY3NTc0ODgxNywic3ViIjoiZGV2ZWxvcGVyL2M4Zjc0MGVkLTMzNmYtOTM0MC1iOGM1LWMyMmYwZWMyYzNlNSIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyIxMzkuMjI4LjExMS4xMjUiXSwidHlwZSI6ImNsaWVudCJ9XX0.qS3J8iJClv8kYFwYr250lbNjTEvn77eQ5-eK9jSOig2nE8xshCQH_VzU2VJYS8tNu_DbZzPmoiCIaMgxJPDxzQ",
+                  "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImEzZjFhOGMxLWUxMTAtNGZhZi1hMTE4LWRmOGI2YmVkYTc4NCIsImlhdCI6MTY3NTc4Nzg5Miwic3ViIjoiZGV2ZWxvcGVyL2M4Zjc0MGVkLTMzNmYtOTM0MC1iOGM1LWMyMmYwZWMyYzNlNSIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyIxMzkuMjI4LjExMS4xMjUiLCIxODAuMjUyLjE2Ni41Il0sInR5cGUiOiJjbGllbnQifV19.4-G8yZmarredfPJYCFq2QZw_rJPrWe9xIL0qiBOGdcaaIRl1YUDhai8M00FIwYeWkMW-ZmDTV6W_W_lJeuWlRw",
               },
         })
         if (!data) throw ({status: 404, msg: 'Player not found'})
