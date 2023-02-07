70c51a41cfea51b9b38f04776fbf0ef52331d8c7
eduardusoldi <eduardusoldi98@gmail.com>
2023-02-07 22:32:42 +0700

    first clean up

--- routes/ytRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const ControllerYoutube = require('../controllers/controllerYoutube')
-const youtubeRoute = express.Router()
-
-youtubeRoute.get('/', ControllerYoutube.fetchYoutubeVideo)
-
-module.exports = youtubeRoute
