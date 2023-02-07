567853eeab2fe6406e94e5cd526ee0690f5acac4
Hamzah Diza Santoso <hamzah.diza@gmail.com>
2023-02-07 13:53:48 +0700

    feat: add register

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,24 @@
+const { User } = require("../models/index");
+const { comparePass } = require("../helpers/bcrypt");
+const { encodeToken } = require("../helpers/jwt");
+
 class UserController {
-  //
+  static async register(req, res, next) {
+    // res.status(201).json("Berhasil");
+    try {
+      const { username, email, password } = req.body;
+      console.log(req.body);
+      let userRegisterData = await User.create({
+        username,
+        email,
+        password,
+      });
+      res.status(201).json({ message: "Register Successfully", userRegisterData });
+    } catch (err) {
+      console.log(err);
+      next(err);
+    }
+  }
 }
 
 module.exports = UserController;
--- routes/routesPost.js
+++ routes/routesPost.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const PostController = require("../controllers/postController");
+const { authentication } = require("../middleware/auth");
 const router = express();
 
-// router.get("/", PostController.getAllPost);
-// router.get("/:id", PostController.getPostById);
+router.use(authentication);
+router.get("/", PostController.getAllPost);
+router.get("/:id", PostController.getPostById);
 // router.post("/:id/like", PostController.postLike);
 
 module.exports = router;
