753942b0ee1b1fe41b2453369ff196ff2868508e
beautydominique <beautydominique6@gmail.com>
2023-02-07 15:47:24 +0700

    create authentication and errorHandler

--- routes/customer.js
+++ routes/customer.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const Controller = require('../controllers/controller')
 const router = express.Router()
-const {authentication} = require("../middlewares/authentication")
-const {authorization} = require('../middlewares/authorization')
+const authentication = require("../middlewares/authentication")
+const authorization = require('../middlewares/authorization')
 
 router.post("/register", Controller.register)
 router.post("/login", Controller.login)
