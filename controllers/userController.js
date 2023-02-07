1b7285ad93ef26a2cf602cf2166be054d5e1e33a
Hamzah Diza Santoso <hamzah.diza@gmail.com>
2023-02-07 14:12:32 +0700

    add feature login

--- controllers/userController.js
+++ controllers/userController.js
@@ -19,6 +19,40 @@ class UserController {
       next(err);
     }
   }
+
+  static async login(req, res, next) {
+    try {
+      const { email, password } = req.body;
+
+      let userLogin = await User.findOne({
+        where: {
+          email: email,
+        },
+      });
+
+      if (!userLogin) {
+        // console.log("Masuk");
+        throw { name: "invalid-login" };
+      }
+
+      let compareResult = comparePass(password, userLogin.password);
+      if (!compareResult) {
+        throw { name: "invalid-login" };
+      }
+
+      // const { id } = userLogin;
+      let access_token = encodeToken({
+        id: userLogin.id,
+      });
+
+      res.status(200).json({
+        access_token,
+      });
+    } catch (err) {
+      console.log(err);
+      next(err);
+    }
+  }
 }
 
 module.exports = UserController;
