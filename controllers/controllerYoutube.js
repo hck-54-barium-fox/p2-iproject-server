3b2f29060562b840d63eec880822bfc4378dbca0
eduardusoldi <eduardusoldi98@gmail.com>
2023-02-07 22:21:11 +0700

    feat: adding controller youtube

---
+++ controllers/controllerYoutube.js
@@ -0,0 +1,21 @@
+const { default: axios } = require("axios");
+const { YTVideo } = require("../models/index");
+
+class ControllerYoutube {
+    static async fetchYoutubeVideo(req, res){
+        try {
+            let { data } = await axios({
+                method: 'get',
+                url: `https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UC_F8DoJf9MZogEOU51TpTbQ&order=date&key=AIzaSyC0GrAcIzB9n1JmhUxSxpmDiKen_zz3HJ4`
+            })
+            let videoData = data.items.map(el => el.id.videoId)
+            res.status(200).json(videoData)
+        } catch (error) {
+            console.log(error)
+            res.status(500).json({
+                message: 'Internal server error'
+            })
+        }
+    }
+}
+module.exports = ControllerYoutube;
