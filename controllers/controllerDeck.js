56ddf028a21896aba8526c47bbd7f763b233ff27
eduardusoldi <eduardusoldi98@gmail.com>
2023-02-08 10:02:30 +0700

    feat: delete my deck

--- controllers/controllerDeck.js
+++ controllers/controllerDeck.js
@@ -17,11 +17,29 @@ class ControllerDeck {
     }
     static async postMyDeck(req, res, next) {
         try {
+            let totalDeck = await Deck.findAll({
+                where: {
+                    UserId: req.user.id
+                }
+            })
+            if (totalDeck.length >= 8) throw ({status: 401, msg: 'Your deck is full!'})
             let dataDeck = await Deck.create({
                 UserId: req.user.id,
                 CardId: req.params.id
             })
             res.status(201).json(dataDeck)
+        } catch (error) {
+            next(error)
+        }
+    }
+    static async deleteMyCardInDeck(req, res, next) {
+        try {
+            await Deck.destroy({
+                where: {
+                    CardId: req.params.id
+                }
+            })
+            res.status(200).json({message: 'Your deck successfully deleted'})
         } catch (error) {
             next(error)
         }
--- routes/deckRoute.js
+++ routes/deckRoute.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const ControllerDeck = require('../controllers/controllerDeck')
+const { authorization } = require('../middleware/auth')
 const deckRoute = express.Router()
 
 deckRoute.get('/', ControllerDeck.getMyDeck)
 deckRoute.post('/:id', ControllerDeck.postMyDeck)
+deckRoute.use(authorization)
+deckRoute.delete('/:id', ControllerDeck.deleteMyCardInDeck)
 
 module.exports = deckRoute
