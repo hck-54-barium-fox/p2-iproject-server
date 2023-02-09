039b09f7d0fc0fffc7c231b2016f0f2ab246a5d9
sammajol63 <samuelsilalahi1996@gmail.com>
2023-02-09 09:16:53 +0700

    fitur getNews

--- router/index.js
+++ router/index.js
@@ -4,6 +4,12 @@ const controller = require('../controller/controller')
 const {authentication} = require('../middleware/author')
 
 router.post('/register', controller.register)
+router.post('/login', controller.login)
+router.post('/loginGoogle', controller.loginGoogle)
+
+router.use(authentication)
+
+router.get('/getNews', controller.getNews)
 
 
 
