b7d64d63d5c789e9484bfee348b997572280f069
cakraJntn <cakrajonatan@gmail.com>
2023-02-09 10:47:17 +0700

    feat: - remaining features

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
+if (process.env.NODE_ENV !== "production") {
+    require('dotenv').config()
+}
+
+
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const cors = require('cors')
 const Controller = require('./Controllers/controller')
 const authentication = require('./middleware/auth')
