e13c4d47e04c69f7a4aa651539356ebe4de334d6
fahmiMaulana1337 <fahmimaulana1337@gmail.com>
2023-02-09 11:06:31 +0700

    update: API DOC

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+    }
 const cors = require('cors')
 const express = require('express');
 const app = express()
-const port = 3000
+const port = process.env.PORT|| 3000
+
 app.use(cors())
 const routes = require('./routes/index');
 
--- routes/index.js
+++ routes/index.js
@@ -6,16 +6,13 @@ const { authentication } = require('../middlewares/auth');
 
 router.post('/register',UserController.register);
 router.post('/login',UserController.login);
-
-
-router.use(authentication)
 router.get('/animal',CustomerController.findAnimal);
 router.get('/cat',CustomerController.findCat);
+router.use(authentication)
 router.get('/product',UserController.getProduct)
 router.get('/pub/checkout',CustomerController.fetchCheckout);
 router.post('/pub/buy/midtrans',CustomerController.checkout);
 router.patch('/pub/paid/:id',CustomerController.paid);
-
 router.delete('/pub/checkout/delete/:id',CustomerController.deleteCheckout);
 router.post('/pub/product/buy/:id',CustomerController.buyProduct);
 
