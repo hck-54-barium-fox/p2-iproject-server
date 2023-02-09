27d360a3bbb715a639df02a942ee0589bc7deed4
devihoesadha <depifebrita@gmail.com>
2023-02-09 07:44:05 +0700

    Fixing all code at server last minute

--- routes/customer.js
+++ routes/customer.js
@@ -1,4 +1,6 @@
 const CustomerController = require("../controller/customerController")
+const { multerIploads } = require("../helper/multer")
+const { custAuthentication } = require("../middleware/auth")
 
 const router = require('express').Router()
 
@@ -6,5 +8,8 @@ router.post("/register", CustomerController.register)
 router.post("/login", CustomerController.login)
 router.post("/ig-login", CustomerController.loginByFacebook)
 
+router.use(custAuthentication)
+router.get("/me", CustomerController.myProfile)
+router.put("/me", multerIploads.single('address'), CustomerController.updateProfile)
 
 module.exports = router
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@ const routerCustomer = require("./customer")
 const routerProduct = require("./product")
 const routerBookmark = require("./bookmark")
 
+router.get("/", (req, res) => {
+    res.send("server is up")
+})
 router.use("/customers", routerCustomer)
 router.use("/products", routerProduct)
 router.use("/bookmarks", routerBookmark)
