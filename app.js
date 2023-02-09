26056984c6e8bc1f9e2658b34e2c3c1c0f2948b7
ahsanfkr <ahsanafkr@gmail.com>
2023-02-09 11:21:28 +0700

    feat: deployment

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ if (process.env.NODE_ENV !== "production") {
 const express = require('express')
 const cors = require('cors')
 const Controller = require('./controllers/controller')
-const port = 3000
+const port = process.env.PORT || 3000
 const app = express()
 const { authentication } = require('./middlewares/auth')
 
